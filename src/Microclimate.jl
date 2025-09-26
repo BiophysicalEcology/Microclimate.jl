af4d7de7bd5d4be990038e3951817fd041deec24
Rafael Schouten <rafaelschouten@gmail.com>
2025-09-26 17:57:07 +1000

    use physical constants from FluidProperties

--- src/Microclimate.jl
+++ src/Microclimate.jl
@@ -1,6 +1,5 @@
 module Microclimate
 
-import PhysicalConstants
 using Interpolations, Statistics, Dates
 using SciMLBase, OrdinaryDiffEqTsit5
 using Unitful, UnitfulMoles
@@ -8,6 +7,7 @@ using ModelParameters, DelimitedFiles
 using SpecialFunctions, StaticArrays
 
 using FluidProperties: wet_air_properties, dry_air_properties, vapour_pressure, enthalpy_of_vaporisation, water_properties, atmospheric_pressure
+using FluidProperties: g_n, σ, atm, R
 using Unitful: °, rad, °C#, K, Pa, kPa, MPa, J, kJ, W, L, g, kg, cm, m, s, hr, d, mol, mmol, μmol, σ, R
 using Interpolations: AbstractInterpolation
 
