b6b8afd33add0014535c41ef87b52f921fa7b84d
mrke <mrke@unimelb.edu.au>
2026-09-02 15:54:33 +1000

    add unit test

--- test/example_constructors.jl
+++ test/example_constructors.jl
@@ -15,6 +15,22 @@ using Test
     @test example_microclimate_problem() isa MicroProblem
 end
 
+@testset "consecutive_days dispatch" begin
+    @test Microclimate.consecutive_days(example_monthly_weather()) == false
+    daily_minmax = DailyMinMaxEnvironment(; forcings = minmax_forcings(;
+        reference_temperature_min = [10, 11, 9]u"°C",
+        reference_temperature_max = [20, 22, 19]u"°C",
+        reference_wind_speed_min = [1, 1, 1]u"m/s",
+        reference_wind_speed_max = [3, 3, 3]u"m/s",
+        reference_humidity_min = [0.3, 0.3, 0.3],
+        reference_humidity_max = [0.8, 0.8, 0.8],
+        cloud_cover_min = [0.1, 0.1, 0.1],
+        cloud_cover_max = [0.5, 0.5, 0.5],
+    ))
+    @test daily_minmax isa DailyMinMaxEnvironment
+    @test Microclimate.consecutive_days(daily_minmax) == true
+end
+
 @testset "example_microclimate_problem solves end-to-end" begin
     out = solve(example_microclimate_problem())
     @test size(out.soil_temperature, 1) == 12 * 24
